d973675d977828ced5151277db645beecccb50fb
Cédryc Ruth <ceruth@epfc.eu>
2024-03-29 14:12:42 +0100

    Traitement des données

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
+<?php
+//Sécurisation (Authorization)
+
+//Déclaration des variables, constantes et fonctions
+$question = "Quel est la couleur du cheval blanc de Napoléon?";
+$reponse = "blanc";
+$resultat = "";
+
+//Traitement des commandes
+if(isset($_GET['btSend'])) {
+	//Valider les données
+	if(!empty($_GET['reponse'])) {	//var_dump('OK');
+		//Réponse correcte ?
+		if(trim($_GET['reponse'])==$reponse) {
+			$resultat = "Bravo!";
+		} else {
+			$resultat = "Dommage...";
+		}
+	} else {	//var_dump('PAS OK');
+		$resultat = "Veuillez fournir une réponse.";
+	}
+}
+?>
 <!doctype html>
 <html lang="fr">
 <head>
@@ -6,18 +29,18 @@
 </head>
 <body>
 <h1>Quizz</h1>
-<p>QUESTION</p>
+<p><?= $question ?></p>
 
 <form action="<?= $_SERVER['PHP_SELF'] ?>" method="get">
 	<fieldset>
 		<label for="reponse">Réponse: </label>
 		<input type="text" name="reponse" id="reponse" required>
 	</fieldset>
-	<button>Envoyer</button>
+	<button name="btSend">Envoyer</button>
 </form>
 
 <div id="resultat">
-	<p>RESULAT</p>
+	<p><?= $resultat ?></p>
 </div>
 </body>
 </html>
